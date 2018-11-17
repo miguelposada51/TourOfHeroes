05cdae80dcd0838a2d755f4a87434cc9c5522cce
miguelposada <miguelposada51@gmail.com>
2018-11-16 19:14:03 -0500

    advance services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,14 +14,28 @@ selectedHero: Hero = {
     name: 'miguelitocamelas'
   };
   herowind = 'Windstorms';
-  heroes = HEROES;
+  //heroes = HEROES;
+  heroes: Hero[];
+  
+  /*getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }*/
+
+  getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 
  	onSelect(hero: Hero): void {
 	  this.selectedHero = hero;
 	}
-   constructor() { }
-  
+  // constructor() { }
+  constructor(private heroService: HeroService) { }
+ 
+ 
+
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
