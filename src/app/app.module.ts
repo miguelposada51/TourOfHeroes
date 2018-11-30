c9c7f3211e744043740034c45316288079aa3de0
Daniel_Pinillos <daniel.pinillos@simplexity.com.co>
2018-11-30 15:42:27 -0500

    HTTP SCRUD Complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgM
 import { HttpClientModule }    from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 
 
@@ -21,6 +22,7 @@ import { InMemoryDataService }  from './in-memory-data.service';
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
+    HeroSearchComponent,
     
   ],
   imports: [
